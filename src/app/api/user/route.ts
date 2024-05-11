2c25bb7e1cae85684d40bc0392d578515f812ff4
brandonhach <specialcambo@gmail.com>
2024-05-11 18:10:59 -0400

    watch for src folder only

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,19 @@
 import { db } from '@/lib/prisma';
-// import { hash } from 'bcrypt';
 import { NextResponse } from 'next/server';
+import argon2 from 'argon2';
+import * as z from 'zod';
+
+// Define schema for body (validation)
+const userSchema = z.object({
+	username: z.string().min(3, 'Username is required').max(20),
+	email: z.string().min(1, 'Email is required').email('Invalid email'),
+	password: z.string().min(1, 'Password is required').min(8, 'Password must have at least 8 characters'),
+});
 
 export async function POST(req: Request) {
 	try {
 		const body = await req.json();
-		const { email, username, password } = body;
+		const { email, username, password } = userSchema.parse(body);
 
 		// check if email already exists
 		const existingUserByEmail = await db.user.findUnique({
@@ -24,16 +32,19 @@ export async function POST(req: Request) {
 		}
 
 		// store new user into db
-		// const hashedPassword = await hash(password, 10);
+		const hashedPassword = await argon2.hash(password);
 		const newUser = await db.user.create({
 			data: {
 				username,
 				email,
-				// password: hashedPassword,
-				password,
+				password: hashedPassword,
 			},
 		});
-		return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
+
+		// remove password from json payload
+		const { password: newUserPassword, ...rest } = newUser;
+
+		return NextResponse.json({ user: rest, message: 'User created successfully' }, { status: 201 });
 	} catch (error) {
 		console.error(error);
 		return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
@@ -41,3 +52,6 @@ export async function POST(req: Request) {
 }
 
 export default POST;
+
+// To verify password
+// const isMatch = await argon2.verify(hash, 'password');
