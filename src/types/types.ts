0012b4df8bfa695a1251761efc0b7b3db09fb9f8
brandonhach <specialcambo@gmail.com>
2024-05-15 17:38:07 -0400

    defined types and zod schema for login and signup

--- src/types/types.ts
+++ src/types/types.ts
@@ -18,7 +18,7 @@ export type TSignUpSchema = z.infer<typeof signUpSchema>;
 
 // SignIn form
 export const signInSchema = z.object({
-	username: z.string().min(1, 'username is required'),
+	email: z.string().min(1, 'Email is required').email('Invalid email'),
 	password: z.string().min(1, 'Password is required').min(8, 'Password must have at least 8 characters'),
 });
 
