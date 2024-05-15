8b82c1aaa2b0da528c05037de32a54e4ed4baa07
brandonhach <specialcambo@gmail.com>
2024-05-15 17:36:52 -0400

    removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/ui/Navbar';
-
-import { usePathname } from 'next/navigation';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
