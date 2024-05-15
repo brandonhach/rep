6e5e550c7dca6c6b91c473c857f3e52d2269423a
brandonhach <specialcambo@gmail.com>
2024-05-15 17:37:27 -0400

    dashboard with sessions

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { auth, signOut } from '@/auth';
 
-const page = () => {
-	return <div>Dashboard</div>;
+const Dashboard = async () => {
+	const session = await auth();
+	return (
+		<div>
+			{JSON.stringify(session)}
+			<form
+				action={async () => {
+					'use server';
+
+					await signOut();
+				}}>
+				<button type='submit' className='btn btn-ghost'>
+					Sign out
+				</button>
+			</form>
+		</div>
+	);
 };
 
-export default page;
+export default Dashboard;
