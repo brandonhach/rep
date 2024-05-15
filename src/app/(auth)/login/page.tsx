ac8564eff0b70b0de93e56466c9f4ed0e87576c8
brandonhach <specialcambo@gmail.com>
2024-05-15 17:37:10 -0400

    use loginform component in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SignInForm from '@/components/auth/SignInForm';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div>
+			<SignInForm></SignInForm>
+		</div>
+	);
 };
 
 export default Login;
