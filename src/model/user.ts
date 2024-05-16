7f7cb33ca96fe56fb5267afa9ef801026b84b955
brandonhach <specialcambo@gmail.com>
2024-05-15 23:28:45 -0400

    second method to grab user by id in user model

--- src/model/user.ts
+++ src/model/user.ts
@@ -8,3 +8,11 @@ export const getUserByEmail = async (email: string) => {
 		return null;
 	}
 };
+export const getUserById = async (id: string) => {
+	try {
+		const user = await db.user.findUnique({ where: { id } });
+		return user;
+	} catch (error) {
+		return null;
+	}
+};
