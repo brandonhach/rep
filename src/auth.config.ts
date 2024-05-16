6f099e4c158179a6b4c089ccee2eb5f9807d50d8
brandonhach <specialcambo@gmail.com>
2024-05-16 10:49:04 -0400

    added two oauth providers

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,13 @@ import Credentials from 'next-auth/providers/credentials';
 import { signInSchema } from './types/types';
 import { getUserByEmail } from './model/user';
 import bcrypt from 'bcryptjs';
+import Discord from 'next-auth/providers/discord';
+import Google from 'next-auth/providers/google';
 
 export default {
 	providers: [
+		Google({ clientId: process.env.GOOGLE_CLIENT_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET }),
+		Discord({ clientId: process.env.DISCORD_CLIENT_ID, clientSecret: process.env.DISCORD_CLIENT_SECRET }),
 		Credentials({
 			async authorize(credentials) {
 				const validatedFields = signInSchema.safeParse(credentials);
