c6bb2cd247e2766983588baa35b1cc3b983e1ca9
brandonhach <specialcambo@gmail.com>
2024-05-16 12:24:16 -0400

    should work but its not working

--- src/auth.ts
+++ src/auth.ts
@@ -13,6 +13,18 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	pages: {
+		signIn: '/login',
+		error: '/error',
+	},
+	events: {
+		async linkAccount({ user }) {
+			await db.user.update({
+				where: { id: user.id },
+				data: { emailVerified: new Date() },
+			});
+		},
+	},
 	callbacks: {
 		async session({ token, session }) {
 			console.log(session);
