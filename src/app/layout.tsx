8b44bd1a7fc8fad683d6c43e8435c9f5dbb25b5f
brandonhach <specialcambo@gmail.com>
2024-05-16 20:49:20 -0400

    wrapped with session provider provided by nextauth so client component can get user auth context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { SessionProvider } from 'next-auth/react';
+import { auth } from '@/auth';
+
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const session = await auth();
 	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
+		<SessionProvider session={session}>
+			<html lang='en'>
+				<body className={inter.className}>{children}</body>
+			</html>
+		</SessionProvider>
 	);
 }
