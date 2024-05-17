e13c5180484b7c8071980548d50c9b95c85857c7
brandonhach <specialcambo@gmail.com>
2024-05-17 14:47:08 -0400

    clean up unused lines of code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 import React from 'react';
-import { db } from '@/lib/prisma';
 import Hero from '@/components/home/Hero';
 import { AuroraBackground } from '@/components/home/ui/Hero/AuroraBG';
 import CTA from '@/components/home/CTA';
 import Navbar from '@/components/ui/Navbar';
-import { usePathname } from 'next/navigation';
 
 const Home = () => {
-	const pathname = usePathname();
-	const isRootPath = pathname === '/';
 	return (
-		<div className=''>
+		<div>
 			<AuroraBackground>
 				<Navbar></Navbar>
 				<Hero></Hero>
