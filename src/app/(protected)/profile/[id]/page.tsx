98372fbb54ccc88ed97d5a8702870a04756cfdfc
brandonhach <specialcambo@gmail.com>
2024-05-16 20:49:43 -0400

    Configure profile page as a server component

--- src/app/(protected)/profile/[id]/page.tsx
+++ src/app/(protected)/profile/[id]/page.tsx
@@ -1,10 +1,21 @@
-'use client';
+import { getUserById } from '@/model/user';
+import { redirect } from 'next/navigation';
+import ProfileComponent from '@/components/profile/Profile';
 
-import { useParams } from 'next/navigation';
+const Profile = async ({ params }: any) => {
+	// Profile page for example is a server component
+	const profile = await getUserById(params.id);
+	if (!profile) {
+		redirect('/dashboard');
+	}
 
-const Profile = () => {
-	const params = useParams();
-	return <div>Profile {params.id}</div>;
+	return (
+		<div>
+			{params.id}
+			{/* Client component */}
+			<ProfileComponent profile={profile} />
+		</div>
+	);
 };
 
 export default Profile;
