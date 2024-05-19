326294908bbc576725bcd3b62f4077dbc8d90513
brandonhach <specialcambo@gmail.com>
2024-05-19 14:02:22 -0400

    edited validation prompts

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,20 +6,17 @@ import z from 'zod';
 
 // SignUp form
 export const signUpSchema = z.object({
-	username: z
-		.string()
-		.min(3, 'Username cannot be less than 3 characters')
-		.max(20, 'Username can only be a max of 20 characters'),
-	email: z.string().min(1, 'Email required').email('Invalid email'),
-	password: z.string().min(1, 'Password required').min(8, '8 characters min.'),
+	username: z.string().min(3, '3 characters min.').max(20, '20 characters max.'),
+	email: z.string().min(1, 'Email required.').email('Invalid email'),
+	password: z.string().min(1, 'Password required.').min(8, '8 characters min.'),
 });
 
 export type TSignUpSchema = z.infer<typeof signUpSchema>;
 
 // SignIn form
 export const signInSchema = z.object({
-	email: z.string().min(1, 'Email required').email('Invalid email'),
-	password: z.string().min(1, 'Password required').min(8, '8 characters min.'),
+	email: z.string().min(1, 'Email required.').email('Invalid email.'),
+	password: z.string().min(1, 'Password required.').min(8, '8 characters min.'),
 });
 
 export type TSignInSchema = z.infer<typeof signInSchema>;
