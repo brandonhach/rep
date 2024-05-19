fc93d948da3bdf80687474f78161edaf35a397ad
brandonhach <specialcambo@gmail.com>
2024-05-19 10:37:49 -0400

    Shorten the validation fields

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,16 +10,16 @@ export const signUpSchema = z.object({
 		.string()
 		.min(3, 'Username cannot be less than 3 characters')
 		.max(20, 'Username can only be a max of 20 characters'),
-	email: z.string().min(1, 'Email is required').email('Invalid email'),
-	password: z.string().min(1, 'Password is required').min(8, 'Password must have at least 8 characters'),
+	email: z.string().min(1, 'Email required').email('Invalid email'),
+	password: z.string().min(1, 'Password required').min(8, '8 characters min.'),
 });
 
 export type TSignUpSchema = z.infer<typeof signUpSchema>;
 
 // SignIn form
 export const signInSchema = z.object({
-	email: z.string().min(1, 'Email is required').email('Invalid email'),
-	password: z.string().min(1, 'Password is required').min(8, 'Password must have at least 8 characters'),
+	email: z.string().min(1, 'Email required').email('Invalid email'),
+	password: z.string().min(1, 'Password required').min(8, '8 characters min.'),
 });
 
 export type TSignInSchema = z.infer<typeof signInSchema>;
