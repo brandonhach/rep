df744810a2c128d549cd098805b15ce221664b6a
brandonhach <specialcambo@gmail.com>
2024-05-18 22:16:34 -0400

    typecasing children & making a reusable container for auth components

--- src/components/auth/ui/FormWrapper.tsx
+++ src/components/auth/ui/FormWrapper.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+'use client';
+import React, { ReactNode } from 'react';
 
-const FormWrapper = () => {
-	return <div>FormWrapper</div>;
+const FormWrapper = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className='w-1/2 h-5/6 border-white border-2 flex flex-row items-center justify-center'>{children}</div>
+	);
 };
 
 export default FormWrapper;
