962553531534c8fe2bfff5310edca22dcf1462d9
brandonhach <specialcambo@gmail.com>
2024-05-24 19:41:05 -0400

    fixed issue with layout where it collided with root layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,20 +1,19 @@
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/ui/Navbar';
+
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default function AuthLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<div className='flex flex-col h-screen w-screen'>
-					<Navbar />
-					<main className='flex flex-grow'>{children}</main>
-				</div>
-			</body>
-		</html>
+		<div className={inter.className}>
+			<div className='flex flex-col h-screen w-screen'>
+				<Navbar />
+				<main className='flex flex-grow'>{children}</main>
+			</div>
+		</div>
 	);
 }
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,18 +1,17 @@
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/ui/Navbar';
+
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default function ProtectedLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<Navbar></Navbar>
-				{children}
-			</body>
-		</html>
+		<div className={inter.className}>
+			<Navbar />
+			{children}
+		</div>
 	);
 }
