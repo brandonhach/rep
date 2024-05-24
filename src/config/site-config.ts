6f163c2e5eb663ed771ac22cfc1808bc119f2c79
brandonhach <specialcambo@gmail.com>
2024-05-24 19:41:48 -0400

    Profile config

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -5,3 +5,10 @@ export const HeroConfig = {
 	searchPlaceholder: placeHolderList,
 	description: '',
 };
+
+export const ProfileConfig = {
+	description: `Ayyy my dudes, it's ya boi M1K3 th3 spac3 pil0t fr0m 3kk reppin' da year 3ravenK. I'm a yung 25yo g flying dem hypersonic scramjetz haulin' cargo n' slushy hydras between Mars, Luna, n' da orbitz. Flyin's in dese ge3ky ge3nes - my space0G pops was bustin' mad thrusterburns way back when he dropped in on Europa in da 2080s, fr fr.
+	`,
+	bgImage:
+		'https://images.unsplash.com/photo-1630839437035-dac17da580d0?q=80&w=1315&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+};
