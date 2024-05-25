1ae2e3f1f22133095d90e3be8e92f1c181648b56
brandonhach <specialcambo@gmail.com>
2024-05-24 23:55:02 -0400

    basic ui layout for the profile card section

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -9,17 +9,17 @@ import { MdOutlineQuestionMark } from 'react-icons/md';
 
 const ProfileCard = ({ profile }: any) => {
 	return (
-		<div className='w-full h-full grid grid-cols-1 grid-rows-3 border-[2px] border-transparent rounded-xl'>
+		<div className='w-full h-full grid grid-cols-1 grid-rows-3 border-[1px] border-transparent rounded-xl border-base-300'>
 			<div className='row-span-2 relative'>
-				<div className='relative w-full h-1/3 flex flex-col justify-end items-center border-transparent rounded-xl'>
-					<Image className='object-cover' src={`${ProfileConfig.bgImage}`} alt='' fill />
+				<div className='relative w-full h-1/3 flex flex-col justify-end items-center '>
+					<Image className='object-cover rounded-t-xl' src={`${ProfileConfig.bgImage}`} alt='' fill />
 					<div className='absolute top-16 flex flex-col justify-center items-center'>
 						<div className='avatar'>
 							<div className='w-32 rounded-full ring ring-black ring-offset-black ring-offset-8'>
 								<Image height={128} width={128} src={`${profile.image}`} alt='' />
 							</div>
 						</div>
-						<h1 className='font-bold text-4xl pt-3 hover:cursor-default'>{profile.name}</h1>
+						<h1 className='font-bold text-4xl pt-3 hover:cursor-default'>{profile.name} 🇺🇸</h1>
 					</div>
 				</div>
 				<div className='pt-32 w-full flex flex-row justify-center items-center'>
@@ -46,7 +46,7 @@ const ProfileCard = ({ profile }: any) => {
 						<div className='stat-desc text-center'>Safe</div>
 					</div>
 				</div>
-				<div className='px-4 font-light leading-5 w-full h-1/3 '>{ProfileConfig.description}</div>
+				<div className='px-4 leading-5 w-full h-1/3 '>{ProfileConfig.description}</div>
 			</div>
 			<div className='flex flex-row items-center justify-center gap-2 pt-32'>
 				<button className='btn btn-outline rounded-xl'>
@@ -60,22 +60,8 @@ const ProfileCard = ({ profile }: any) => {
 					<MdCompareArrows />
 				</button>
 			</div>
-
-			<div className='row-span-2 flex-col items-center justify-center p-4'>
-				<div className='w-full pb-10'>
-					<label className='form-control w-full max-w-xs'>
-						<div className='label'>
-							<span className='label-text'>Verification</span>
-							<span className='label-text-alt tooltip' data-tip={`What's this?`}>
-								<button className=''>
-									<MdOutlineQuestionMark />
-								</button>
-							</span>
-						</div>
-						<input type='text' placeholder='Enter code' className='input input-bordered w-full max-w-xs' />
-					</label>
-				</div>
-				<div className='pb-10'>
+			<div className='flex flex-row items-center justify-center pt-10'>
+				<div className='px-4'>
 					<h3 className='font-bold text-lg'>Linked Accounts</h3>
 					<div>
 						<button>
@@ -98,6 +84,25 @@ const ProfileCard = ({ profile }: any) => {
 								<FaXTwitter />
 							</div>
 						</button>
+					</div>
+				</div>
+				<div className='row-span-2 flex-col items-center justify-center p-4'>
+					<div className='w-full '>
+						<label className='form-control w-full max-w-xs'>
+							<div className='label'>
+								<span className='label-text'>Verification</span>
+								<span className='label-text-alt tooltip' data-tip={`What's this?`}>
+									<button className=''>
+										<MdOutlineQuestionMark />
+									</button>
+								</span>
+							</div>
+							<input
+								type='text'
+								placeholder='Enter code'
+								className='input input-bordered w-full max-w-xs rounded-xl'
+							/>
+						</label>
 					</div>
 				</div>
 			</div>
