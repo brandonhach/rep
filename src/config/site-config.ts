00c57de3f8a18a6052e47e1dac8d98378a80b018
brandonhach <specialcambo@gmail.com>
2024-05-25 00:14:37 -0400

    edited and added more info

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -7,8 +7,15 @@ export const HeroConfig = {
 };
 
 export const ProfileConfig = {
-	description: `Ayyy my dudes, it's ya boi M1K3 th3 spac3 pil0t fr0m 3kk reppin' da year 3ravenK. I'm a yung 25yo g flying dem hypersonic scramjetz haulin' cargo n' slushy hydras between Mars, Luna, n' da orbitz. Flyin's in dese ge3ky ge3nes - my space0G pops was bustin' mad thrusterburns way back when he dropped in on Europa in da 2080s, fr fr.
+	description: `Yo, I'm a Rolex seller and collector. Always on the lookout for those collector's editions, so if you got one, hit me up. Besides that, I’ve got a stash of virtual items up for grabs – rare skins, in-game gear, you name it. If you’re into Rolex deals, we can handle that over the counter too. Just slide into my DMs and let’s make it happen!
 	`,
 	bgImage:
-		'https://images.unsplash.com/photo-1630839437035-dac17da580d0?q=80&w=1315&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		'https://images.unsplash.com/photo-1582414004129-a955c6087f5e?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+};
+
+export const MoodboardConfig = {
+	pic1: 'https://images.unsplash.com/photo-1658696987956-bba35e5a7724?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	pic2: 'https://images.unsplash.com/photo-1627860284764-c92502f39afb?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	pic3: 'https://images.unsplash.com/photo-1639444100617-810c1df608a8?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	pic4: 'https://images.unsplash.com/photo-1598468852802-8871fdd45069?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhlcm1lc3xlbnwwfHwwfHx8MA%3D%3D',
 };
