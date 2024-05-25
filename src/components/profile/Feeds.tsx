f7273b2747b13fa95cdfed4240e12ffc39338bb9
brandonhach <specialcambo@gmail.com>
2024-05-24 23:51:16 -0400

    setup tabs layout to let users click on each sub component

--- src/components/profile/Feeds.tsx
+++ src/components/profile/Feeds.tsx
@@ -1,21 +1,49 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import Rep from './ui/Feeds/Rep';
+import Comments from './ui/Feeds/Comments';
+import Follows from './ui/Feeds/Follows';
+import Contacts from './ui/Feeds/Contacts';
 
 const Feeds = () => {
+	const [activeTab, setActiveTab] = useState('rep');
+
+	const handleTabClick = (tab: React.SetStateAction<string>) => {
+		setActiveTab(tab);
+	};
 	return (
-		<div className='w-full h-full'>
+		<div className='w-full h-full overflow-hidden'>
 			<div role='tablist' className='tabs tabs-lifted'>
-				<a role='tab' className='tab'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'rep' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('rep')}>
 					Rep
 				</a>
-				<a role='tab' className='tab tab-active'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'comments' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('comments')}>
 					Comments
 				</a>
-				<a role='tab' className='tab'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'follows' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('follows')}>
 					Follows
 				</a>
-				<a role='tab' className='tab'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'contacts' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('contacts')}>
 					Contacts
 				</a>
+			</div>
+			<div className='w-full h-full p-4'>
+				{activeTab === 'rep' && <Rep></Rep>}
+				{activeTab === 'comments' && <Comments></Comments>}
+				{activeTab === 'follows' && <Follows></Follows>}
+				{activeTab === 'contacts' && <Contacts></Contacts>}
 			</div>
 		</div>
 	);
--- src/components/profile/Showcase.tsx
+++ src/components/profile/Showcase.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import Moodboard from './ui/Showcase/Moodboard';
+import Achievements from './ui/Showcase/Achievements';
+import Posts from './ui/Showcase/Posts';
 
 const Showcase = () => {
+	const [activeTab, setActiveTab] = useState('moodboard');
+
+	const handleTabClick = (tab: React.SetStateAction<string>) => {
+		setActiveTab(tab);
+	};
 	return (
-		<div className='w-full h-full'>
+		<div className='w-full h-full overflow-hidden'>
 			<div role='tablist' className='tabs tabs-lifted'>
-				<a role='tab ' className='tab tab-active'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'moodboard' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('moodboard')}>
 					Moodboard
 				</a>
-				<a role='tab' className='tab '>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'achievements' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('achievements')}>
 					Achievements
 				</a>
-				<a role='tab' className='tab'>
+				<a
+					role='tab'
+					className={`tab ${activeTab === 'posts' ? 'tab-active' : ''}`}
+					onClick={() => handleTabClick('posts')}>
 					Posts
 				</a>
+			</div>
+			<div className='w-full h-full p-4'>
+				{activeTab === 'moodboard' && <Moodboard></Moodboard>}
+				{activeTab === 'achievements' && <Achievements></Achievements>}
+				{activeTab === 'posts' && <Posts></Posts>}
 			</div>
 		</div>
 	);
