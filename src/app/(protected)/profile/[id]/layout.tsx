713164d74626adc7cb1e6a84e50649cc7514cd63
brandonhach <specialcambo@gmail.com>
2024-05-25 00:12:48 -0400

    kept html tag within layout for now

--- src/app/(protected)/profile/[id]/layout.tsx
+++ src/app/(protected)/profile/[id]/layout.tsx
@@ -3,17 +3,19 @@ import Navbar from '@/components/ui/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function ProtectedLayout({
+export default function ProfileLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
 		<html lang='en'>
-			<div className={inter.className}>
-				<Navbar />
-				{children}
-			</div>
+			<body className={inter.className}>
+				<div className='flex flex-col h-screen w-screen'>
+					<Navbar />
+					<main className='flex flex-grow'>{children}</main>
+				</div>
+			</body>
 		</html>
 	);
 }
