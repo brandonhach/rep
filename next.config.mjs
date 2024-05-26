545b251015cb81901c5b1be1436dbcfee963a76e
brandonhach <specialcambo@gmail.com>
2024-05-25 20:13:54 -0400

    enable domains for temp pics

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['lh3.googleusercontent.com', 'cdn.discordapp.com', 'images.unsplash.com'],
+		domains: [
+			'lh3.googleusercontent.com',
+			'cdn.discordapp.com',
+			'images.unsplash.com',
+			'banner2.cleanpng.com',
+			'png.pngtree.com',
+		],
 	},
 };
 
