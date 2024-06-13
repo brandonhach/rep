8603b144a7d9f3e5ad2ebe7ee4cd3f4921351c83
brandonhach <specialcambo@gmail.com>
2024-06-13 00:09:36 -0400

    removed setting ui mockup component

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,12 +1,11 @@
 import { getUserById } from '@/model/user';
 import { redirect } from 'next/navigation';
-import Settings from '@/components/settings/Settings';
 import InnerNav from '@/components/settings/InnerNav';
 const SettingsPage = () => {
 	return (
 		<div className='flex flex-row w-full h-full items-start justify-center'>
 			<div>
-				<InnerNav />
+				<InnerNav></InnerNav>
 			</div>
 		</div>
 	);
