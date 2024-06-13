fd3ed06ce9535af661d3891d9c54f2c9f619ed4d
Orin Sparks <orinsparks@gmail.com>
2024-06-13 01:15:39 -0400

    Added post type radio buttons. Edit trade post is WIP.

--- src/actions/trade-posts/edit-trade-post.ts
+++ src/actions/trade-posts/edit-trade-post.ts
@@ -1,14 +1,18 @@
 'use server';
 import * as z from 'zod';
 import { db } from '@/lib/prisma';
-import { addTradePostSchema } from '@/types/types';
+import { editradePostSchema } from '@/types/types';
 import { revalidatePath } from 'next/cache';
 import {undefined} from "zod";
 
 export const editTradePost = async (formData: FormData) => {
     const profileId = formData.get('profileId');
     const userId = formData.get('userId');
-    const content = formData.get('content');
+    const title = formData.get('title');
+    const image = formData.get('image');
+    const description = formData.get('description');
+    const price = formData.get('price');
+    const postType = formData.get('postType');
 
     await db.tradePost.update({
         data: {
@@ -17,6 +21,8 @@ export const editTradePost = async (formData: FormData) => {
             title: title as string,
             image: image as string,
             description: description as string,
+            price: price as string,
+            postType:postType as string,
         }
     });
 
--- src/components/profile/ui/Showcase/Posts.tsx
+++ src/components/profile/ui/Showcase/Posts.tsx
@@ -8,16 +8,13 @@ import {useSession} from "next-auth/react";
 import {MdAddBox, MdEditSquare} from "react-icons/md";
 import {TTradePost} from "@/types/types";
 import {addTradePost} from "@/actions/trade-posts/add-trade-post";
+import {editTradePost} from "@/actions/trade-posts/edit-trade-post";
 
 
 const Posts = ({ params, tradePosts }: any) => {
 	const session = useSession();
 	return (
 		<div className='w-full h-full overflow-auto'>
-			<button className="btn rounded-xl"
-					onClick={() => (document.getElementById('addTradePost_modal') as HTMLDialogElement).showModal()}>
-				<MdAddBox />
-			</button>
 			<div className='w-full h-full grid grid-cols-2 auto-rows-[328px] overflow-auto'>
 				{tradePosts.map((tradePost: TTradePost) => {
 					return (
@@ -33,12 +30,18 @@ const Posts = ({ params, tradePosts }: any) => {
 								</p>
 								<button className='btn rounded-xl absolute -top-2 -right-2'
 										onClick={() => (document.getElementById('editTradePost_modal') as HTMLDialogElement).showModal()}>
-									<MdEditSquare />
+									<MdEditSquare/>
 								</button>
 							</DirectionAwareHover>
 						</div>
 					);
 				})}
+				<div className="flex justify-center items-center bg-stone-950 col-span-1 m-4 rounded-xl relative overflow-hidden">
+					<button className="btn rounded-xl"
+							onClick={() => (document.getElementById('addTradePost_modal') as HTMLDialogElement).showModal()}>
+						<MdAddBox/> Create new post
+					</button>
+				</div>
 			</div>
 			<div className='w-full z-50 h-1/6 flex flex-row items-end justify-between px-3'>
 				<dialog id='addTradePost_modal' className='modal'>
@@ -64,27 +67,24 @@ const Posts = ({ params, tradePosts }: any) => {
 									Price:
 									<input type="text" name='price' className="grow" placeholder="daisy@site.com"/>
 								</label>
-								<label className="input input-bordered flex items-center gap-2 rounded-xl">
-									Post Type:
-									<input type="text" name='postType' className="grow" placeholder="type"/>
-								</label>
-								{/*<div className='flex flex-row items-center justify-evenly'>*/}
-								{/*	<div className="form-control">*/}
-								{/*		<label className="label cursor-pointer gap-4">*/}
-								{/*			<span className="label-text">Selling</span>*/}
-								{/*			<input type="radio" name="radio-10"*/}
-								{/*				   className="radio checked:bg-green-500"*/}
-								{/*				   checked/>*/}
-								{/*		</label>*/}
-								{/*	</div>*/}
-								{/*	<div className="form-control">*/}
-								{/*		<label className="label cursor-pointer gap-4">*/}
-								{/*			<span className="label-text">Buying</span>*/}
-								{/*			<input type="radio" name="radio-10" className="radio checked:bg-red-500"*/}
-								{/*				   checked/>*/}
-								{/*		</label>*/}
-								{/*	</div>*/}
-								{/*</div>*/}
+								<div className='flex flex-row items-center justify-evenly'>
+									<div className="form-control">
+										<label className="label cursor-pointer gap-4">
+											<span className="label-text">Selling</span>
+											<input type="radio" name="postType" defaultValue="Selling"
+												   className="radio checked:bg-green-500"
+												   checked/>
+										</label>
+									</div>
+									<div className="form-control">
+										<label className="label cursor-pointer gap-4">
+											<span className="label-text">Buying</span>
+											<input type="radio" name="postType" defaultValue="Buying"
+												   className="radio checked:bg-red-500"
+												   checked/>
+										</label>
+									</div>
+								</div>
 							</label>
 							<button
 								className='btn btn-outline rounded-xl btn-sm'
@@ -102,70 +102,65 @@ const Posts = ({ params, tradePosts }: any) => {
 					</form>
 				</dialog>
 			</div>
-			{/*<div className='w-full z-50 h-1/6 flex flex-row items-end justify-between px-3'>*/}
-			{/*	<dialog id='editTradePost_modal' className='modal'>*/}
-			{/*		<div className='modal-box rounded-xl'>*/}
-			{/*			<form action={editTradePost} className='px-2 flex flex-col gap-4 items-end size-full'>*/}
-			{/*				<label className='form-control size-full gap-4'>*/}
-			{/*					Edit this post:*/}
-			{/*					<input type='hidden' name='profileId' value={params.id}/>*/}
-			{/*					<input type='hidden' name='userId' value={session.data?.user.id!}/>*/}
-			{/*					<label className="input input-bordered flex items-center gap-2 rounded-xl">*/}
-			{/*						Title:*/}
-			{/*						<input type="text" name='title' className="grow" placeholder="Daisy"/>*/}
-			{/*					</label>*/}
-			{/*					<label className="input input-bordered flex items-center gap-2 rounded-xl">*/}
-			{/*						Image:*/}
-			{/*						<input type="text" name='image' className="grow" placeholder="daisy@site.com"/>*/}
-			{/*					</label>*/}
-			{/*					<textarea*/}
-			{/*						className='textarea textarea-bordered w-full h-24 resize-none rounded-xl whitespace-pre-line'*/}
-			{/*						name='description'*/}
-			{/*						placeholder='Provide a discription for your post...'></textarea>*/}
-			{/*					<label className="input input-bordered flex items-center gap-2 rounded-xl">*/}
-			{/*						Price:*/}
-			{/*						<input type="text" name='price' className="grow" placeholder="daisy@site.com"/>*/}
-			{/*					</label>*/}
-			{/*					<div className='flex flex-row items-center justify-evenly'>*/}
-			{/*						<div className="form-control">*/}
-			{/*							<label className="label cursor-pointer gap-4">*/}
-			{/*								<span className="label-text">Selling</span>*/}
-			{/*								<input type="radio" name="radio-10"*/}
-			{/*									   className="radio checked:bg-green-500"*/}
-			{/*									   checked/>*/}
-			{/*							</label>*/}
-			{/*						</div>*/}
-			{/*						<div className="form-control">*/}
-			{/*							<label className="label cursor-pointer gap-4">*/}
-			{/*								<span className="label-text">Buying</span>*/}
-			{/*								<input type="radio" name="radio-10" className="radio checked:bg-red-500"*/}
-			{/*									   checked/>*/}
-			{/*							</label>*/}
-			{/*						</div>*/}
-			{/*					</div>*/}
-			{/*				</label>*/}
-			{/*				<button*/}
-			{/*					className='btn btn-outline rounded-xl btn-sm'*/}
-			{/*					type='submit'*/}
-			{/*					onClick={() => {*/}
-			{/*						(document.getElementById('addTradePost_modal') as HTMLDialogElement).close();*/}
-			{/*						// mutate(url);*/}
-			{/*					}}>*/}
-			{/*					Submit*/}
-			{/*				</button>*/}
-			{/*			</form>*/}
-			{/*		</div>*/}
-			{/*		<form method='dialog' className='modal-backdrop'>*/}
-			{/*			<button>close</button>*/}
-			{/*		</form>*/}
-			{/*	</dialog>*/}
-			{/*	<div className='join'>*/}
-			{/*		<button className='join-item btn btn-sm btn-active'>1</button>*/}
-			{/*		<button className='join-item btn btn-sm'>2</button>*/}
-			{/*		<button className='join-item btn btn-sm'>3</button>*/}
-			{/*		<button className='join-item btn btn-sm'>4</button>*/}
-			{/*	</div>*/}
-			{/*</div>*/}
+			<div className='w-full z-50 h-1/6 flex flex-row items-end justify-between px-3'>
+				<dialog id='editTradePost_modal' className='modal'>
+					<div className='modal-box rounded-xl'>
+						<form action={editTradePost} className='px-2 flex flex-col gap-4 items-end size-full'>
+							<label className='form-control size-full gap-4'>
+								Edit this post:
+								<input type='hidden' name='profileId' value={params.id}/>
+								<input type='hidden' name='userId' value={session.data?.user.id!}/>
+								<label className="input input-bordered flex items-center gap-2 rounded-xl">
+									Title:
+									<input type="text" name='title' className="grow" placeholder="Daisy"/>
+								</label>
+								<label className="input input-bordered flex items-center gap-2 rounded-xl">
+									Image:
+									<input type="text" name='image' className="grow" placeholder="daisy@site.com"/>
+								</label>
+								<textarea
+									className='textarea textarea-bordered w-full h-24 resize-none rounded-xl whitespace-pre-line'
+									name='description'
+									placeholder='Provide a discription for your post...'></textarea>
+								<label className="input input-bordered flex items-center gap-2 rounded-xl">
+									Price:
+									<input type="text" name='price' className="grow" placeholder="daisy@site.com"/>
+								</label>
+								<div className='flex flex-row items-center justify-evenly'>
+									<div className="form-control">
+										<label className="label cursor-pointer gap-4">
+											<span className="label-text">Selling</span>
+											<input type="radio" name="postType" defaultValue="Selling"
+												   className="radio checked:bg-green-500"
+												   checked/>
+										</label>
+									</div>
+									<div className="form-control">
+										<label className="label cursor-pointer gap-4">
+											<span className="label-text">Buying</span>
+											<input type="radio" name="postType" defaultValue="Buying"
+												   className="radio checked:bg-red-500"
+												   checked/>
+										</label>
+									</div>
+								</div>
+							</label>
+							<button
+								className='btn btn-outline rounded-xl btn-sm'
+								type='submit'
+								onClick={() => {
+									(document.getElementById('addTradePost_modal') as HTMLDialogElement).close();
+									// mutate(url);
+								}}>
+								Submit
+							</button>
+						</form>
+					</div>
+					<form method='dialog' className='modal-backdrop'>
+						<button>close</button>
+					</form>
+				</dialog>
+			</div>
 		</div>
 );
 };
