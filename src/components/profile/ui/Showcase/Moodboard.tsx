52a490146c326a7498936916efa4a9cf5772c46d
Orin Sparks <orinsparks@gmail.com>
2024-06-24 23:04:34 -0400

    Added infinite scroll functionality. Styling is WIP.

--- src/components/profile/ui/Showcase/Moodboard.tsx
+++ src/components/profile/ui/Showcase/Moodboard.tsx
@@ -5,6 +5,7 @@ import { TMoodboard } from '@/types/types';
 import { db } from '@/lib/prisma';
 import { addMoodboard } from '@/actions/moodboards/add-moodboards';
 import {getMoodboards} from "@/actions/moodboards/get-moodboards";
+import {undefined} from "zod";
 
 const MOODS_PER_PAGE = 8;
 
@@ -14,19 +15,45 @@ const Moodboard = ({ params, moodboards }: any) => {
 	const [offset, setOffset] = useState(MOODS_PER_PAGE);
 	const [moods, setMoods] = useState<TMoodboard[]>(moodboards);
 	const [hasMoreData, setHasMoreData] = useState(true);
+	const scrollTrigger = useRef(null);
 
-	const loadMoreMoods = async () => {
+	const loadMoreMoods = useCallback (async () => {
 		if (hasMoreData) {
 			const apiMoods = await getMoodboards(params.id, offset, MOODS_PER_PAGE)
 
 			if (apiMoods.length == 0) {
 				setHasMoreData(false);
+			} else {
+				setMoods((prevMoods) => [...prevMoods, ...apiMoods]);
+				setOffset((prevOffset) => prevOffset + MOODS_PER_PAGE);
 			}
-
-			setMoods((prevMoods) => [...prevMoods, ...apiMoods]);
-			setOffset((prevOffset) => prevOffset + MOODS_PER_PAGE);
 		}
-	}
+	}, [params.id, offset, hasMoreData]);
+
+	useEffect(() => {
+		if (typeof window === "undefined" || !window.IntersectionObserver) {
+			return;
+		}
+
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting) {
+					loadMoreMoods();
+				}
+			},
+			{threshold: 0.5}
+		);
+
+		if (scrollTrigger.current) {
+			observer.observe(scrollTrigger.current);
+		}
+
+		return () => {
+			if (scrollTrigger.current) {
+				observer.unobserve(scrollTrigger.current);
+			}
+		};
+	}, [loadMoreMoods]);
 
 	return (
 		<div className='w-full h-full overflow-auto'>
@@ -42,6 +69,13 @@ const Moodboard = ({ params, moodboards }: any) => {
 						</div>
 					))
 				)}
+				<div>
+					{hasMoreData ? (
+						<div ref={scrollTrigger}>Loading...</div>
+					) : (
+						<p>You have reached the bottom.</p>
+					)}
+				</div>
 			</div>
 			<div className='w-full flex flex-row items-end justify-between'>
 			{(moods.length === 0 && params.id === session.data?.user.id || params.id === session.data?.user.id ? (
@@ -49,10 +83,6 @@ const Moodboard = ({ params, moodboards }: any) => {
 					<button className="btn rounded-xl"
 						onClick={() => (document.getElementById('moodboard_modal') as HTMLDialogElement).showModal()}>
 						Add a moodboard
-					</button>
-					<button className="btn rounded-xl"
-						onClick={loadMoreMoods}>
-						Load more moods
 					</button>
 					<dialog id="moodboard_modal" className="modal">
 						<div className="modal-box rounded-xl">
