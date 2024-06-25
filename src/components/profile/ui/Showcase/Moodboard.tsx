ae3dfeb5899c1eb588f5dc1d1fc6d8f3f27cecbf
Orin Sparks <orinsparks@gmail.com>
2024-06-25 00:33:57 -0400

    Added styling changes to the loading message. Added styling changes to the add new mood button to better align with infinite scrolling functionality. Fixed modal form not resetting after submission. Loading after adding new mood is WIP.

--- src/components/profile/ui/Showcase/Moodboard.tsx
+++ src/components/profile/ui/Showcase/Moodboard.tsx
@@ -6,6 +6,7 @@ import { db } from '@/lib/prisma';
 import { addMoodboard } from '@/actions/moodboards/add-moodboards';
 import {getMoodboards} from "@/actions/moodboards/get-moodboards";
 import {undefined} from "zod";
+import {MdAddBox} from "react-icons/md";
 
 const MOODS_PER_PAGE = 8;
 
@@ -55,8 +56,15 @@ const Moodboard = ({ params, moodboards }: any) => {
 		};
 	}, [loadMoreMoods]);
 
+	const handleAddMood = () => {
+		const form = document.getElementById('addMood_form') as HTMLFormElement
+		const modal = document.getElementById('addMood_modal') as HTMLDialogElement
+		form.reset();
+		modal.showModal();
+	}
+
 	return (
-		<div className='w-full h-full overflow-auto'>
+		<div className='w-full h-full overflow-auto relative'>
 			<div className='w-full h-full grid grid-cols-4 auto-rows-[328px] overflow-auto'>
 				{moodboards.length === 0 ? (
 					<div className='w-full h-full flex flex-row items-start justify-start'>
@@ -69,28 +77,29 @@ const Moodboard = ({ params, moodboards }: any) => {
 						</div>
 					))
 				)}
-				<div>
-					{hasMoreData ? (
+				{hasMoreData ? (
+					<div className='flex justify-center items-center'>
 						<div ref={scrollTrigger}>Loading...</div>
-					) : (
-						<p>You have reached the bottom.</p>
-					)}
-				</div>
+					</div>
+				) : null}
+				{moods.length === 0 && params.id === session.data?.user.id || params.id === session.data?.user.id ? (
+					<div className="absolute top-6 right-10 opacity-65 rounded-xl overflow-hidden hover:opacity-100">
+						<button className="btn text-xl rounded-xl" onClick={handleAddMood}>
+							<MdAddBox/>
+						</button>
+					</div>
+				) : null}
 			</div>
 			<div className='w-full flex flex-row items-end justify-between'>
 			{(moods.length === 0 && params.id === session.data?.user.id || params.id === session.data?.user.id ? (
 					<div className='flex-1'>
-					<button className="btn rounded-xl"
-						onClick={() => (document.getElementById('moodboard_modal') as HTMLDialogElement).showModal()}>
-						Add a moodboard
-					</button>
-					<dialog id="moodboard_modal" className="modal">
+					<dialog id='addMood_modal' className="modal">
 						<div className="modal-box rounded-xl">
 							<form method="dialog">
 								{/*Closes the modal box*/}
 								<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 							</form>
-							<form action={addMoodboard} className='px-2 py-2 flex flex-col gap-4 items-end size-full'>
+							<form id='addMood_form' action={addMoodboard}  className='px-2 py-2 flex flex-col gap-4 items-end size-full'>
 								<label className='form-control size-full'>
 									<input type='hidden' name='profileId' value={params.id} />
 									<input type='hidden' name='userId' value={session.data?.user.id!} />
@@ -105,7 +114,7 @@ const Moodboard = ({ params, moodboards }: any) => {
 									className='btn btn-outline rounded-xl btn-sm'
 									type='submit'
 									onClick={() => {
-										(document.getElementById('moodboard_modal') as HTMLDialogElement).close();
+										(document.getElementById('addMood_modal') as HTMLDialogElement).close();
 									}}>
 									Add Moodboard
 								</button>
