2586989eabd77d3e11c4295d700054ef70615bd0
brandonhach <specialcambo@gmail.com>
2024-07-09 15:01:48 -0400

    changed button on top right

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 	const session = useSession();
 	const pathname = usePathname();
 	return (
-		<div className='flex flex-row items-center justify-between px-8 h-14 mt-2 w-screen'>
+		<div className='flex flex-row items-center justify-between px-8 h-14 mt-2 w-screen z-50'>
 			<div className=' text-xl h-full flex flex-col items-center justify-center'>
 				<Link href={'/'}>
 					<Image alt='Logo' src={'/logo/rep.png'} width={150} height={150}></Image>
@@ -81,9 +81,14 @@ const Navbar = () => {
 					</div>
 				) : (
 					<>
-						<li className='btn btn-outline rounded-xl'>
-							<Link href={'/signup'}>Start now</Link>
-						</li>
+						<button className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 z-50'>
+							<span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#f59e0b_0%,#fef3c7_50%,#d97706_100%)]' />
+							<span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl hover:bg-neutral-900/90 duration-700'>
+								<Link href={'/signup'}>
+									<p className='text-xs px-2'>Get Started</p>
+								</Link>
+							</span>
+						</button>
 					</>
 				)}
 			</div>
