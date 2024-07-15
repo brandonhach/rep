222b3a8bcf9d418cf8ab8d36665c17c0c65b0538
brandonhach <specialcambo@gmail.com>
2024-07-15 10:54:22 -0400

    static to dynamic fetch

--- src/components/profile/ui/Feeds/Rep.tsx
+++ src/components/profile/ui/Feeds/Rep.tsx
@@ -3,9 +3,9 @@ import Flag from 'react-world-flags';
 import { IoDocumentTextOutline } from 'react-icons/io5';
 import { PiThumbsUpLight } from 'react-icons/pi';
 import Image from 'next/image';
-import { RepConfig } from '@/config/site-config';
+import { TRep } from '@/types/types';
 
-const Rep = () => {
+const Rep = ({ reps }: { reps: TRep[] }) => {
 	return (
 		<div className='h-full w-full overflow-y-auto'>
 			<table className='table table-pin-rows'>
@@ -19,8 +19,7 @@ const Rep = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{/* row 1 */}
-					{RepConfig.Reps.map((user, index) => {
+					{reps.map((rep, index) => {
 						return (
 							<>
 								<tr key={index}>
@@ -29,20 +28,20 @@ const Rep = () => {
 											<div className='avatar'>
 												<div className='avatar rounded-full w-14 ring-black ring-offset-black ring-offset-1'>
 													<Image
-														src={`${user.image}`}
+														src={`${rep.user.image}`}
 														height={48}
 														width={48}
 														alt='avatar'></Image>
 												</div>
 											</div>
 											<div className='flex flex-col items-start justify-center'>
-												<div className='font-bold pb-1'>{user.name}</div>
+												<div className='font-bold pb-1'>{rep.user.name}</div>
 												<div className='flex flex-row items-center justify-start gap-2'>
-													<Flag code={`${user.country}`} height='30' width='30' />
+													<Flag code={`${rep.user.country}`} height='30' width='30' />
 
 													<div className='badge badge-ghost badge-lg rounded-xl flex flex-row justify-center items-center text-center gap-1'>
 														<span className='text-green-500 text-md font-bold'>
-															{user.rep}
+															{rep.rating}
 														</span>
 														<PiThumbsUpLight className='text-lg text-green-500' />
 													</div>
@@ -51,10 +50,10 @@ const Rep = () => {
 										</div>
 									</td>
 									<td>
-										<p>{user.business}</p>
+										<p>{rep.description}</p>
 										<br />
 										<div className='flex flex-row gap-2'>
-											{user.transaction.map((item, index) => {
+											{rep.keywords.map((item, index) => {
 												return (
 													<>
 														<div
@@ -68,7 +67,7 @@ const Rep = () => {
 										</div>
 									</td>
 									<td>
-										{user.rating ? (
+										{rep.rating ? (
 											<h3 className='text-green-600 font-extrabold text-2xl'>+rep</h3>
 										) : (
 											<h3 className='text-red-600 font-extrabold text-2xl'>-rep</h3>
