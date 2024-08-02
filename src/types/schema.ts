26c103f1cc4f29e9450e10f94dfe09f2765b66c8
brandonhach <specialcambo@gmail.com>
2024-08-02 11:47:59 -0400

    define zod schema with infer type

---
+++ src/types/schema.ts
@@ -0,0 +1,16 @@
+import { boolean, z } from 'zod';
+
+// const logSchema = z.object({ title: z.string(), details: z.string() });
+
+export const repSchema = z.object({
+	description: z.string().min(4, 'This description is too short.').max(120, 'This description is too long.'),
+	rating: z.enum(['true', 'false']), // boolean type
+	keywords: z.union([
+		z.string().transform((str) => str.split(',').filter(Boolean)),
+		z.array(z.string().min(2, 'Keyword too short.').max(10, 'Keyword no longer than 10 characters.')),
+	]),
+	profileId: z.string().min(1),
+	userId: z.string().min(1),
+});
+
+export type Rep = z.infer<typeof repSchema>;
