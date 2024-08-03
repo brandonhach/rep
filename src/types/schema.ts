031e603612b09b99c14dba51cd0a46f360f8c9f8
brandonhach <specialcambo@gmail.com>
2024-08-03 11:12:13 -0400

    revise zod schema for rep

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,14 +1,18 @@
-import { boolean, z } from 'zod';
+import { z } from 'zod';
 
 // const logSchema = z.object({ title: z.string(), details: z.string() });
 
 export const repSchema = z.object({
-	description: z.string().min(4, 'This description is too short.').max(120, 'This description is too long.'),
-	rating: z.enum(['true', 'false']), // boolean type
-	keywords: z.union([
-		z.string().transform((str) => str.split(',').filter(Boolean)),
-		z.array(z.string().min(2, 'Keyword too short.').max(10, 'Keyword no longer than 10 characters.')),
-	]),
+	description: z
+		.string()
+		.min(1, 'Description is required.')
+		.max(120, 'Max length for description is 120 characters.'),
+	rating: z.union([z.enum(['true', 'false']), z.literal(null)]).refine((val) => val !== null, {
+		message: 'Select a rating.',
+	}), // boolean type
+	keywords: z
+		.array(z.string().min(1, 'Empty keyword.').max(10, 'Max length for keyword is 10 characters.'))
+		.max(5, 'Max keyword accepted is 5.'),
 	profileId: z.string().min(1),
 	userId: z.string().min(1),
 });
