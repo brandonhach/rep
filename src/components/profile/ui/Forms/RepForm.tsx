d51c29953286bdb21e0455167117ab5a7c8c4dc3
brandonhach <specialcambo@gmail.com>
2024-08-03 11:11:35 -0400

    documented and set state for button based on validation

--- src/components/profile/ui/Forms/RepForm.tsx
+++ src/components/profile/ui/Forms/RepForm.tsx
@@ -3,7 +3,7 @@ import { addRep } from '@/actions/rep/add-rep';
 import { Rep, repSchema } from '@/types/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useSession } from 'next-auth/react';
-import { ChangeEvent, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import BasicForm from './RepFormWizard/BasicForm';
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
@@ -11,6 +11,8 @@ import LogForm from './RepFormWizard/LogForm';
 
 const RepForm = ({ params }: { userId: string; params: any }) => {
 	const session = useSession();
+
+	// Handles next and prev tab
 	const [activeTab, setActiveTab] = useState(0);
 	const handlePrevClick = () => {
 		setActiveTab((prevTab) => (prevTab > 0 ? prevTab - 1 : prevTab));
@@ -20,17 +22,61 @@ const RepForm = ({ params }: { userId: string; params: any }) => {
 		setActiveTab((prevTab) => (prevTab < 3 ? prevTab + 1 : prevTab));
 	};
 
+	/**
+	 * Zod + React-hook-form. Since this is a wizard form, it will be initialized as methods instead of the
+	 * traditional destructure version. See doc for that.
+	 */
+
 	const methods = useForm<Rep>({
 		resolver: zodResolver(repSchema),
 		mode: 'onChange',
 	});
 
-	const { register, reset } = methods;
+	const {
+		register,
+		reset,
+		formState: { isValid },
+		trigger,
+	} = methods;
 
 	const processForm: SubmitHandler<Rep> = async (data) => {
-		addRep(data);
+		const result = addRep(data);
+		if ((await result).success) {
+			reset();
+		}
 	};
+
+	// For debugging & troubleshooting. Remove this from rendering for Prod.
 	const onInvalid = (errors: any) => console.error(errors);
+
+	/**
+	 * Validation:
+	 * Disable next btn after clicking & validation fails.
+	 * Btn also reverts to !disabled if validation is fixed.
+	 */
+	const [validationFailed, setValidationFailed] = useState(false);
+
+	const validateAndProceed = async () => {
+		// Trigger validation for all fields
+		const result = await trigger();
+
+		if (result) {
+			// If validation passes, proceed to the next tab
+			setValidationFailed(false);
+			handleNextClick();
+		} else {
+			// If validation fails, update state to disable the button
+			setValidationFailed(true);
+		}
+	};
+
+	useEffect(() => {
+		if (isValid) {
+			setValidationFailed(false);
+		}
+	}, [isValid]);
+
+	console.log(methods.getValues());
 
 	return (
 		<FormProvider {...methods}>
@@ -47,22 +93,25 @@ const RepForm = ({ params }: { userId: string; params: any }) => {
 						<input type='hidden' {...register('profileId')} value={params.id} />
 						<input type='hidden' {...register('userId')} value={session.data?.user.id} />
 						{activeTab === 0 && <BasicForm></BasicForm>}
-						{activeTab === 1 && <LogForm></LogForm>}
+						{/* {activeTab === 1 && <LogForm></LogForm>} */}
 
 						<div className='w-full flex flex-row items-center justify-between gap-10 pt-20'>
 							<button
 								className={`btn-outline rounded-xl btn-lg flex flex-row items-center ${
 									activeTab === 0 ? 'invisible duration-0' : 'hover: duration-500'
 								} `}
-								onClick={handlePrevClick}>
+								onClick={handlePrevClick}
+								type='button'>
 								<GrFormPrevious />
 								Prev
 							</button>
 							<button
-								className={`btn-outline rounded-xl btn-lg flex flex-row items-center ${
+								className={`btn rounded-xl btn-lg flex flex-row items-center ${
 									activeTab === 2 ? 'invisible hover: duration-0' : ' hover: duration-500'
 								}`}
-								onClick={handleNextClick}>
+								onClick={validateAndProceed}
+								disabled={validationFailed}
+								type='button'>
 								Next <GrFormNext />
 							</button>
 							<button
@@ -72,7 +121,6 @@ const RepForm = ({ params }: { userId: string; params: any }) => {
 								onClick={() => {
 									(document.getElementById('rep_modal') as HTMLDialogElement).close();
 									setActiveTab(0);
-									reset();
 								}}>
 								Submit form
 							</button>
