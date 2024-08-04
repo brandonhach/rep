00e546db4ea533ed0c16392e5b1631651ef80586
brandonhach <specialcambo@gmail.com>
2024-08-04 09:54:29 -0400

    error handling with type assertion

--- src/components/profile/ui/Forms/RepFormWizard/LogForm.tsx
+++ src/components/profile/ui/Forms/RepFormWizard/LogForm.tsx
@@ -7,24 +7,33 @@ const LogForm = () => {
 		formState: { errors },
 		watch,
 	} = useFormContext();
+
+	// Type assertion for this log schema
+	const logErrors = errors.log as { title?: { message?: string }; description?: { message?: string } } | undefined;
+
 	return (
 		<div className='size-full'>
 			<h1 className='text-4xl font-bold'>Logs</h1>
 			<p className='text-lg pb-4'>Fill out the basic details about the transaction & experience.</p>
 			<div className=' flex flex-col gap-4 items-end w-full h-full'>
-				{/* description */}
-
+				{/* Title */}
+				{logErrors?.title?.message && (
+					<p className='text-sm text-red-400'>{String(logErrors?.title?.message)}</p>
+				)}
 				<input
 					type='text'
-					placeholder='Enter a short sentence summarizing this transaction'
+					placeholder='Title'
 					className='input input-bordered w-full rounded-md indicator'
-					{...register('title')}
+					{...register('log.title')}
 				/>
+				{/* description */}
+				{logErrors?.description?.message && (
+					<p className='text-sm text-red-400'>{String(logErrors?.description?.message)}</p>
+				)}
 				<textarea
 					placeholder='Document your experience and provide transaction evidence.'
-					{...register('log.summary')}
 					className='input input-bordered w-full rounded-md indicato resize-none h-32 px-4 py-2'
-					{...register('log.details')}
+					{...register('log.description')}
 				/>
 
 				<input type='file' className='file-input file-input-ghost w-full max-w-xs' />
