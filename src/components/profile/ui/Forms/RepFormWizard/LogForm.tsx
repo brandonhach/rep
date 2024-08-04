4facaf7ecc8e5f84ce6b3f1b57a24bd8e351d376
brandonhach <specialcambo@gmail.com>
2024-08-04 11:15:12 -0400

    implement toast to display error msg

--- src/components/profile/ui/Forms/RepFormWizard/LogForm.tsx
+++ src/components/profile/ui/Forms/RepFormWizard/LogForm.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { cToast } from './cToast';
 
 const LogForm = () => {
 	const {
 		register,
 		formState: { errors },
-		watch,
 	} = useFormContext();
 
 	// Type assertion for this log schema
 	const logErrors = errors.log as { title?: { message?: string }; description?: { message?: string } } | undefined;
+
+	// This is use to listen for any errors and display them as a toast using react-hot-toast
+	useEffect(() => {
+		if (logErrors?.title) {
+			cToast(String(logErrors?.title?.message));
+		}
+		if (logErrors?.description) {
+			cToast(String(logErrors?.description?.message));
+		}
+	}, [errors, logErrors?.description, logErrors?.title]);
 
 	return (
 		<div className='size-full'>
@@ -17,22 +27,20 @@ const LogForm = () => {
 			<p className='text-lg pb-4'>Fill out the basic details about the transaction & experience.</p>
 			<div className=' flex flex-col gap-4 items-end w-full h-full'>
 				{/* Title */}
-				{logErrors?.title?.message && (
-					<p className='text-sm text-red-400'>{String(logErrors?.title?.message)}</p>
-				)}
 				<input
 					type='text'
 					placeholder='Title'
-					className='input input-bordered w-full rounded-md indicator'
+					className={`input input-bordered w-full rounded-md indicator ${
+						logErrors?.title ? 'duration-700 border-error' : 'border'
+					}`}
 					{...register('log.title')}
 				/>
 				{/* description */}
-				{logErrors?.description?.message && (
-					<p className='text-sm text-red-400'>{String(logErrors?.description?.message)}</p>
-				)}
 				<textarea
 					placeholder='Document your experience and provide transaction evidence.'
-					className='input input-bordered w-full rounded-md indicato resize-none h-32 px-4 py-2'
+					className={`input input-bordered w-full rounded-md indicato resize-none h-32 px-4 py-2 ${
+						logErrors?.description ? 'duration-700 border-error' : 'border'
+					}`}
 					{...register('log.description')}
 				/>
 
