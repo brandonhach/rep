9cfec165ad55a370a6ceaf357e81e1f45dec7477
brandonhach <specialcambo@gmail.com>
2024-08-04 09:52:23 -0400

    pass a boolean based on validation state to repform through prop

--- src/components/profile/ui/Forms/RepFormWizard/ConfirmForm.tsx
+++ src/components/profile/ui/Forms/RepFormWizard/ConfirmForm.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import { lorem } from '@/config/site-config';
-import { useForm, useFormContext } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { RepConfirm, repConfirmSchema } from '@/types/schema';
 
-const ConfirmForm = () => {
+const ConfirmForm = ({ setConfirmValid }: { setConfirmValid: React.Dispatch<React.SetStateAction<boolean>> }) => {
 	const {
 		register,
-		formState: { errors },
+		formState: { errors, isValid },
+		trigger,
 		watch,
 	} = useForm<RepConfirm>({ resolver: zodResolver(repConfirmSchema) });
+
+	const accuracy = watch('accuracy');
+	const understanding = watch('understanding');
+
+	useEffect(() => {
+		setConfirmValid(isValid && accuracy && understanding);
+		if (isValid && accuracy && understanding === false) {
+			trigger();
+		}
+	}, [accuracy, isValid, setConfirmValid, trigger, understanding]);
+
 	return (
 		<div className='size-full overflow-hidden'>
 			<h1 className='text-4xl font-bold'>Confirmation</h1>
@@ -19,30 +32,20 @@ const ConfirmForm = () => {
 					<p>{lorem}</p>
 				</div>
 				<label className='label cursor-pointer gap-2'>
-					{errors.accuracy?.message && (
+					{/* {errors.accuracy?.message && (
 						<p className='text-sm text-red-400'>{String(errors.accuracy.message)}</p>
-					)}
+					)} */}
 					<span className='label-text'>I confirm that the information provided is accurate.</span>
-					<input
-						type='checkbox'
-						{...register('accuracy', {
-							required: 'You must confirm that the information provided is accurate.',
-						})}
-					/>
+					<input type='checkbox' {...register('accuracy')} />
 				</label>
 				<label className='label cursor-pointer gap-2'>
-					{errors.understanding?.message && (
+					{/* {errors.understanding?.message && (
 						<p className='text-sm text-red-400'>{String(errors.understanding.message)}</p>
-					)}
+					)} */}
 					<span className='label-text'>
 						I understand that false information may result in account termination.
 					</span>
-					<input
-						type='checkbox'
-						{...register('understanding', {
-							required: 'You must confirm that the information provided is accurate.',
-						})}
-					/>
+					<input type='checkbox' {...register('understanding')} />
 				</label>
 			</div>
 		</div>
