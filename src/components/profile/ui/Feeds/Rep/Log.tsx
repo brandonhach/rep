77ee6bc1808c495b771dd009fa81ce03804ff959
brandonhach <specialcambo@gmail.com>
2024-08-10 14:23:47 -0400

    finished basic ui mockup for the log modal

--- src/components/profile/ui/Feeds/Rep/Log.tsx
+++ src/components/profile/ui/Feeds/Rep/Log.tsx
@@ -1,43 +1,43 @@
 'use client';
-import { TLog, TRep } from '@/types/types';
+import { TRep } from '@/types/types';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FcAddImage } from 'react-icons/fc';
+import { SiGoogledocs } from 'react-icons/si';
 
 const Log = ({ rep }: { rep: TRep }) => {
-	const imageClass = 'size-32 border-white border-[1px] hover:cursor-pointer rounded-md ';
 	return (
-		<div className='size-full flex flex-col items-start justify-start rounded-md gap-4'>
-			{/* Reputation detail */}
-			<div className='p-4 w-full h-[5.5rem] flex flex-row items-start justify-between bg-base-200/50 rounded-lg'>
-				<div className='flex flex-row gap-4'>
-					{/* Users  */}
-					<div className='flex flex-row'>
-						<div className='flex flex-row items-center justify-start gap-1'>
-							<Link href={`/profile/${rep.userId}`} className='tooltip' data-tip={`${rep.user.name}`}>
-								<Image
-									src={`${rep.user.image}`}
-									height={48}
-									width={48}
-									alt='avatar'
-									className='rounded-full'></Image>
-							</Link>
+		<div className='size-full flex flex-col gap-4'>
+			<div className='w-full h-[10%] col-span-4 row-span-6 rounded-xl bg-base-200/50 p-2 flex flex-col items-start justify-start gap-4'>
+				{/* Title & keywords */}
+				<div className='flex flex-row items-start justify-between w-full'>
+					<div className='flex flex-row gap-2'>
+						{/* Avatars */}
+						<div className='flex flex-row'>
+							<div className='flex flex-row items-center justify-start gap-1'>
+								<Link href={`/profile/${rep.userId}`} className='tooltip' data-tip={`${rep.user.name}`}>
+									<Image
+										src={`${rep.user.image}`}
+										height={48}
+										width={48}
+										alt='avatar'
+										className='rounded-full'></Image>
+								</Link>
+							</div>
+							<div className='flex flex-row items-center justify-start gap-1'>
+								<Link
+									href={`/profile/${rep.profileId}`}
+									className='tooltip'
+									data-tip={`${rep.profile.name}`}>
+									<Image
+										src={`${rep.profile.image}`}
+										height={48}
+										width={48}
+										alt='avatar'
+										className='rounded-full'></Image>
+								</Link>
+							</div>
 						</div>
-						<div className='flex flex-row items-center justify-start gap-1'>
-							<Link
-								href={`/profile/${rep.profileId}`}
-								className='tooltip'
-								data-tip={`${rep.profile.name}`}>
-								<Image
-									src={`${rep.profile.image}`}
-									height={48}
-									width={48}
-									alt='avatar'
-									className='rounded-full'></Image>
-							</Link>
-						</div>
-					</div>
-					{/* Title & keywords */}
-					<div>
 						<h1>
 							<span className='text-2xl font-bold'>{rep.log.title}</span>
 							<div>
@@ -55,51 +55,50 @@ const Log = ({ rep }: { rep: TRep }) => {
 							</div>
 						</h1>
 					</div>
-				</div>
-				{/* Rating */}
-				<div>
-					{rep.rating ? (
-						<h3 className='text-green-600 font-extrabold text-3xl'>+rep</h3>
-					) : (
-						<h3 className='text-red-600 font-extrabold text-3xl'>-rep</h3>
-					)}
+
+					{/* Rating */}
+					<div>
+						{rep.rating ? (
+							<h3 className='text-green-600 font-extrabold text-3xl'>+rep</h3>
+						) : (
+							<h3 className='text-red-600 font-extrabold text-3xl'>-rep</h3>
+						)}
+					</div>
 				</div>
 			</div>
-
-			{/* Log Details */}
 			<div
 				role='tablist'
-				className='tabs tabs-lifted tabs-lg w-full h-2/5 flex flex-col items-start justify-start bg-base-200/50 rounded-lg'>
-				<div role='tab' className='tab tab-active w-fit '>
+				className='tabs tabs-lifted tabs-lg w-full h-[80%] flex flex-col items-start justify-start bg-base-200/50 rounded-lg'>
+				<div role='tab' className='h-30 tab tab-active w-fit'>
 					Log Description
 				</div>
-				<div className='size-full pt-4 pb-12 '>
+				<div className='size-full overflow-y-scroll scrollbar-hide'>
 					{/* Description */}
-					<div className='w-full h-full overflow-y-scroll overflow-x-hidden px-4 '>
-						<p className='font-normal text-lg whitespace-pre-line'>{rep.log.description}</p>
-					</div>
+					<p className='font-normal text-lg whitespace-pre-line px-4 py-2'>{rep.log.description}</p>
 				</div>
 			</div>
-
-			{/* Evidence */}
-			<div
-				role='tablist'
-				className='tabs tabs-lifted tabs-lg w-full h-2/5  flex flex-col items-start justify-start bg-base-200/50 rounded-lg'>
-				<div role='tab' className='tab tab-active w-fit '>
-					Evidence
+			{/* Files */}
+			<div className='w-full h-[10%] flex flex-row items-center justify-start gap-2'>
+				{/* 1 */}
+				<div className='w-fit  p-1 rounded-lg hover:cursor-pointer bg-base-200/70'>
+					<p className='font-semibold flex flex-row gap-1'>
+						<FcAddImage className='text-xl' />
+						screenshots184124.png <span className='text-neutral-100 font-thin px-1'>3.2MB</span>
+					</p>
 				</div>
-				<div className='size-full overflow-y-hidden flex flex-row justify-between items-start gap-10 px-4 pt-4'>
-					{/* Image */}
-					<div className='w-2/3 flex flex-row items-start justify-start gap-4 overflow-y-hidden overflow-x-scroll'>
-						<div className={`${imageClass}`}></div>
-						<div className={`${imageClass}`}></div>
-						<div className={`${imageClass}`}></div>
-					</div>
-					{/* Files */}
-					<div className='w-1/3 flex flex-row items-start justify-start gap-4 overflow-y-hidden overflow-x-scroll'>
-						<div className={`${imageClass}`}></div>
-						<div className={`${imageClass}`}></div>
-					</div>
+				{/* 2*/}
+				<div className='w-fit  p-1 rounded-lg hover:cursor-pointer bg-base-200/70 '>
+					<p className='font-semibold flex flex-row gap-1'>
+						<SiGoogledocs className='text-xl text-blue-400' />
+						chatlog.doc <span className='text-neutral-100 font-thin px-1'>13.2MB</span>
+					</p>
+				</div>
+				{/* 3 */}
+				<div className='w-fit  p-1 rounded-lg hover:cursor-pointer bg-base-200/70'>
+					<p className='font-semibold flex flex-row gap-1'>
+						<FcAddImage className='text-xl' />
+						screenshots1123231.png <span className='text-neutral-100 font-thin px-1'>2.4MB</span>
+					</p>
 				</div>
 			</div>
 		</div>
@@ -107,3 +106,7 @@ const Log = ({ rep }: { rep: TRep }) => {
 };
 
 export default Log;
+
+{
+	/* Users  */
+}
